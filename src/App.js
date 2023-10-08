3139741d566d4fbb55b046d37586ba927124afee
Antoinette-Cordis <133567517+Antoinette-Cordis@users.noreply.github.com>
2023-10-08 11:33:06 -0700

    Created responsive Weather App Using React.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container">
+          <div className="weather-app">
+            <form id="search-input" class="mb-3">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Type a City"
+                    className="form-control"
+                    id="city-input"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="Submit"
+                    value="search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="row">
+              <div className="col-6">
+                <h1 id="city"></h1>
+                <ul id="clear">
+                  <li id="date">
+                    <span></span>
+                  </li>
+                  <li id="description"></li>
+                </ul>
+              </div>
+              <div className="col-6">
+                <span>
+                  <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                    alt="clear"
+                    id="icon"
+                  />
+                </span>
+
+                <strong id="temperature"></strong>
+                <span>°C</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <ul className="weather">
+                  <li>
+                    Humidity:<span id="humidity"></span>%
+                  </li>
+                  <li>
+                    Wind:<span id="wind"></span>Km/hr
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="weather-forecast" id="forecast">
+              <div className="row"></div>
+            </div>
+          </div>
+        </div>
+        <div className="name">
+          <a
+            href="https://github.com/Antoinette-Cordis/WEATHER-APP-PROJECT"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open source
+          </a>
+          <small> by: Antoinette Cordis Amauba</small>
+        </div>
+
+        <Weather city="" />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Weather.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [weather, setWeather] = useState("");
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(true);
+
+  function showTemperature(response) {
+    setLoaded(false);
+
+    setWeather({
+      humidity: response.data.main.humidity,
+      temperature: Math.round(response.data.main.temp),
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      date: formatDate(response.data.dt * 1000),
+    });
+    showForecast(response.data.coord);
+  }
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+    return (
+      <h3>
+        `The ${weather} in ${city} is warm`
+      </h3>
+    );
+  }
+  function formatDate(timestamp) {
+    let now = new Date(timestamp);
+    let hours = now.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+    let day = days[now.getDay()];
+    let year = now.getFullYear();
+    return `${year},${day} ${hours}:${minutes}`;
+  }
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    return days[day];
+  }
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHTML =
+          forecastHTML +
+          `<div class="col-2">
+              <div class="weather-forecast-dates">${formatDay(
+                forecastDay.dt
+              )}</div>
+              <img
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
+                alt=""
+                width="40"
+              />
+              <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
+              </div>  
+            </div>
+            `;
+      }
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+  function showForecast(coordinates) {
+    console.log(coordinates);
+    let Apikey = "bd5b4461863eddaa6ced0a0a67989e0a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${Apikey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+  }
+
+  function search(city) {
+    let Apikey = "bd5b4461863eddaa6ced0a0a67989e0a";
+    let Apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}&units=metric`;
+    axios.get(Apiurl).then(showTemperature);
+  }
+  function searchForm(event) {
+    event.preventDefault();
+    let cityElement = document.querySelector("#city-input");
+    search(cityElement.value);
+  }
+  search("Nigeria");
+  let form =
+    (document.querySelector("#search-input"),
+    (
+      <form onSubmit={searchForm}>
+        <input
+          type="search"
+          placeholder="Type a City"
+          autoFocus="on"
+          onChange={updateCity}
+        />
+        <input type="submit" value="search" />
+      </form>
+    ));
+  if (loaded) return;
+  <div>{form}</div>;
+}
